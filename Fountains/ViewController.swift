a0065624057af2428c18b4ce033f8c25c9688752
James La Marre <jameslamarre@gmail.com>
2015-07-01 17:57:53 +0100

    Initial Commit

---
+++ Fountains/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fountains
+//
+//  Created by James La Marre on 7/1/15.
+//  Copyright (c) 2015 James La Marre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
